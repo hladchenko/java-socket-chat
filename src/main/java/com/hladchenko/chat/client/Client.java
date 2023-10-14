f8d0df47107f00c14931f62cf33970c5a7776765
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-14 18:44:43 +0300

    Fix client

--- src/main/java/com/hladchenko/chat/client/Client.java
+++ src/main/java/com/hladchenko/chat/client/Client.java
@@ -1,29 +1,28 @@
 package com.hladchenko.chat.client;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
 
     public static void main(String[] args) {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line = "";
+        while (!line.equals("q")) {
+            try (Socket socket = new Socket("127.0.0.1", 8899)) {
 
-        try (Socket socket = new Socket("127.0.0.1", 8899)) {
+                OutputStream outputStream = socket.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                PrintWriter printWriter = new PrintWriter(outputStreamWriter);
 
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            PrintWriter printWriter = new PrintWriter(outputStreamWriter);
-
-            printWriter.print("It works!");
-            printWriter.flush();
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                System.out.print("Enter your message: ");
+                line = bufferedReader.readLine();
+                printWriter.println(line);
+                printWriter.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-
-
     }
 }
